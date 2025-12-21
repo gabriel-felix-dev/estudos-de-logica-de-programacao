57eff6841e096ca18d00656603b906d2e6ca5977
Gabriel Felix <gabrielfelix099@hotmail.com>
2025-12-21 20:49:18 -0300

    logica-de-programacao: Primeiro exercício de lógica de programação em C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        double nota1, nota2, nota3, nota4, media;
+
+        Console.WriteLine("Digite a primeira nota: ");
+        nota1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a segunda nota: ");
+        nota2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a terceira nota: ");
+        nota3 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quarta nota: ");
+        nota4 = double.Parse(Console.ReadLine());
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        Console.WriteLine($"A média das notas é: {media}");
+    }
+}
